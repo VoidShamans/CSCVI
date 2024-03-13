bf08f8df2ef96573976b1f5eb973c133d9762dc6
Clyde <clydesdaleog@gmail.com>
2024-03-13 15:28:29 -0400

    Lots of CSS and new HTML added trying to link new

--- web_project/urls.py
+++ web_project/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name='home'),
+    path('', views.home, name='home'),  
     path('Home/', views.home, name='home'),
+    path('Video/', views.video, name='video'),
 ]
--- web_project/views.py
+++ web_project/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 
-
 def home(request):
     return render(request, 'web_project/Home.html')
+
+def video(request):
+    return render(request, 'web_project/Video.html')
